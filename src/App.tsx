8b6372256fb7098b9883537bb6268a614e67f1ce
Ahmed Raza <ahmedr.0331@gmail.com>
2025-03-16 03:06:47 +0500

    minor changed

--- src/App.tsx
+++ src/App.tsx
@@ -220,7 +220,9 @@ function MainContent({
           {isChecking ? (
              <div className="flex justify-center items-center flex-col">
              <h2 className="text-3xl font-bold text-center mb-12">You can now access dashboard</h2>
-             <button  onClick={() => window.location.href ='https://app.unitain.net/'} className="text-white bg-primary-600 px-8 py-4 rounded-lg font-semibold text-lg hover:bg-primary-500 transition-colors duration-200 shadow-lg hover:shadow-xl transform hover:-translate-y-1">
+             <button
+               onClick={() => window.location.href ='https://app.unitain.net/'} 
+               className="text-white bg-primary-600 px-8 py-4 rounded-lg font-semibold text-lg hover:bg-primary-500 transition-colors duration-200 shadow-lg hover:shadow-xl transform hover:-translate-y-1">
                Go to dashboard
              </button>
            </div>
