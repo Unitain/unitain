2110318c28832b69d1d24fdd0be5c450d44ab15e
Ahmed Raza <ahmedr.0331@gmail.com>
2025-04-18 12:54:22 +0500

    updated icons

--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {Search, Eye, Filter, Clock, X, Check} from "lucide-react"
+import {Search,Download, Eye, Filter, Clock, X, Check} from "lucide-react"
 import { supabase } from "../supabase";
 
 const Documents = () => {
@@ -245,15 +245,11 @@ const Documents = () => {
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                   <div className="flex justify-end space-x-2">
-                    <button className="p-1 text-gray-400 hover:text-blue-600 transition-colors">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clipRule="evenodd" />
-                      </svg>
+                    <button className="p-1 text-blue-600 transition-colors">
+                      <Eye />
                     </button>
                     <button className="p-1 text-gray-400 hover:text-yellow-500 transition-colors">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
+                      <Download />
                     </button>
                   </div>
                 </td>
